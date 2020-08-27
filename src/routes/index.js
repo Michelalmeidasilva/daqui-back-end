201f3e56896ce0eda96d718b24b72ef084184b05
Michel Silva <michelalmeidasilva@outlook.pt>
2020-08-27 16:02:12 -0300

    feat: added import pessoa-fisica-routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import Router from 'koa-router'
 import users from './users-router'
 import me from './me-router'
+import pessoasfisicas from './pessoas-fisicas-router'
 
 const router = new Router()
 const api = new Router()
 
+api.use(pessoasfisicas)
 api.use(users)
 api.use(me)
 
